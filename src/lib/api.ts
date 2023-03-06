5201597489688d1d5a7019571db19f27d4340a40
rafaalmeida1 <rafaprof312@gmail.com>
2023-03-06 13:49:10 -0300

    fix: URL to api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://localhost:3000/api",
-  baseURL: "https://balance-app-opal.vercel.app/api",
+  baseURL: "https://balance-app-nine.vercel.app/api",
 })
